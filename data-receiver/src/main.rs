3247be31205908e1c0708e46025ef0c6c48e2daa
Dion Dokter <diondokter@gmail.com>
2024-01-23 07:56:55 +0100

    More code

--- data-receiver/src/main.rs
+++ data-receiver/src/main.rs
@@ -2,17 +2,131 @@
 #![no_std]
 
 use defmt_rtt as _;
-use embassy_stm32::Config;
-use embassy_time::Timer;
+use embassy_stm32::{
+    adc::Adc,
+    peripherals::{ADC1, PA5},
+    rcc::{
+        AHBPrescaler, APBPrescaler, Hse, HseMode, Pll, PllMul, PllPDiv, PllPreDiv, PllSource,
+        Sysclk,
+    },
+    time::hz,
+    Config,
+};
+use embassy_time::{Duration, Timer};
 use panic_probe as _;
+
+const DATA_RATE: u64 = 2400;
 
 #[embassy_executor::main]
 async fn main(_spawner: embassy_executor::Spawner) -> ! {
-    let config = Config::default();
-    let _p = embassy_stm32::init(config);
+    let mut config = Config::default();
+
+    config.rcc.hse = Some(Hse {
+        freq: hz(8_000_000),
+        mode: HseMode::Oscillator,
+    });
+    config.rcc.hsi = false;
+    config.rcc.sys = Sysclk::PLL1_P;
+    config.rcc.pll_src = PllSource::HSE;
+    config.rcc.pll = Some(Pll {
+        prediv: PllPreDiv::DIV4,
+        mul: PllMul::MUL180,
+        divp: Some(PllPDiv::DIV2),
+        divq: None,
+        divr: None,
+    });
+    config.rcc.ahb_pre = AHBPrescaler::DIV1;
+    config.rcc.apb1_pre = APBPrescaler::DIV4;
+    config.rcc.apb2_pre = APBPrescaler::DIV2;
+
+    let dp = embassy_stm32::init(config);
+
+    let mut adc = Adc::new(dp.ADC1, &mut embassy_time::Delay);
+    let mut adc_pin = dp.PA5;
 
     loop {
-        defmt::info!("Hello World!");
-        Timer::after_secs(1).await;
+        let mut buffer = [0; 1024];
+
+        let len = receive_data(&mut adc, &mut adc_pin, &mut buffer).await;
+
+        defmt::info!("{}", buffer[..len.min(1024)]);
     }
+}
+
+async fn receive_data(adc: &mut Adc<'_, ADC1>, adc_pin: &mut PA5, buffer: &mut [u8]) -> usize {
+    buffer.fill(0);
+
+    adc.set_resolution(embassy_stm32::adc::Resolution::EightBit);
+    adc.set_sample_time(embassy_stm32::adc::SampleTime::Cycles3);
+
+    let mut previous_state = None;
+
+    defmt::info!("Waiting for start of transmission");
+    while (adc.read(adc_pin) as u8) < 25 {
+        Timer::after(Duration::from_hz(DATA_RATE * 4 * 4)).await;
+    }
+
+    for (byte_index, byte) in buffer.iter_mut().enumerate() {
+        for i in (0..8).step_by(2) {
+            Timer::after(Duration::from_hz(DATA_RATE * 4 * 4)).await;
+
+            let new_state = loop {
+                let mut adc_value = adc.read(adc_pin) as u8;
+
+                let adc_value = loop {
+                    let next_adc_value = adc.read(adc_pin) as u8;
+                    if adc_value.abs_diff(next_adc_value) < 20 {
+                        break next_adc_value;
+                    } else {
+                        adc_value = next_adc_value;
+                    }
+                };
+
+                let read_state = if adc_value < 25 {
+                    return byte_index;
+                } else if adc_value > 40 && adc_value < 60 {
+                    0
+                } else if adc_value > 90 && adc_value < 110 {
+                    1
+                } else if adc_value > 140 && adc_value < 160 {
+                    2
+                } else if adc_value > 190 && adc_value < 210 {
+                    3
+                } else if adc_value > 240 {
+                    4
+                } else {
+                    continue;
+                };
+
+                if let Some(previous_state) = previous_state {
+                    if read_state != previous_state {
+                        break read_state;
+                    }
+                } else {
+                    break read_state;
+                }
+            };
+
+            let bits = match previous_state {
+                Some(previous_state) => {
+                    let change_amount = if new_state > previous_state {
+                        new_state - previous_state
+                    } else {
+                        new_state + 5 - previous_state
+                    };
+
+                    change_amount - 1
+                }
+                None => new_state,
+            };
+
+            defmt::trace!("{}, {}, {}", previous_state, new_state, bits);
+
+            previous_state = Some(new_state);
+
+            *byte |= bits << i
+        }
+    }
+
+    buffer.len()
 }
--- data-transmitter/src/main.rs
+++ data-transmitter/src/main.rs
@@ -2,17 +2,90 @@
 #![no_std]
 
 use defmt_rtt as _;
-use embassy_stm32::Config;
-use embassy_time::Timer;
+use embassy_stm32::{
+    dac::{DacChannel, Value}, peripherals::{DAC, DMA1_CH5}, rcc::{AHBPrescaler, APBPrescaler, Hse, HseMode, Pll, PllMul, PllPDiv, PllPreDiv, PllSource, Sysclk}, time::hz, Config
+};
+use embassy_time::{Ticker, Timer};
 use panic_probe as _;
+
+static TRANSMISSION: &[u8] = include_bytes!("../transmission.bin");
 
 #[embassy_executor::main]
 async fn main(_spawner: embassy_executor::Spawner) -> ! {
-    let config = Config::default();
-    let _p = embassy_stm32::init(config);
+    let mut config = Config::default();
+
+    config.rcc.hse = Some(Hse {
+        freq: hz(8_000_000),
+        mode: HseMode::Oscillator,
+    });
+    config.rcc.hsi = false;
+    config.rcc.sys = Sysclk::PLL1_P;
+    config.rcc.pll_src = PllSource::HSE;
+    config.rcc.pll = Some(Pll {
+        prediv: PllPreDiv::DIV4,
+        mul: PllMul::MUL180,
+        divp: Some(PllPDiv::DIV2),
+        divq: None,
+        divr: None,
+    });
+    config.rcc.ahb_pre = AHBPrescaler::DIV1;
+    config.rcc.apb1_pre = APBPrescaler::DIV4;
+    config.rcc.apb2_pre = APBPrescaler::DIV2;
+
+    let dp = embassy_stm32::init(config);
+
+    let mut dac = embassy_stm32::dac::DacChannel::new(dp.DAC, dp.DMA1_CH5, dp.PA4);
+
+    dac.set(Value::Bit8(0));
+    dac.enable();
 
     loop {
-        defmt::info!("Hello World!");
+        defmt::info!("Transmitting data now");
+        transmit_data(
+            &mut dac,
+            &[0, 1],
+            2400,
+        )
+        .await;
         Timer::after_secs(1).await;
     }
+}
+
+const BIT_PATTERNS: [Value; 5] = [
+    Value::Bit8(50),
+    Value::Bit8(100),
+    Value::Bit8(150),
+    Value::Bit8(200),
+    Value::Bit8(250),
+];
+
+async fn transmit_data(
+    dac: &mut DacChannel<'static, DAC, 1, DMA1_CH5>,
+    data: &[u8],
+    data_rate: u64,
+) {
+    let mut previous_state = None;
+    let mut ticker = Ticker::every(embassy_time::Duration::from_hz(data_rate * 4));
+
+    for byte in data {
+        for i in (0..8u8).step_by(2) {
+            let bits = (byte & (0x03 << i)) >> i;
+
+            match previous_state.as_mut() {
+                Some(previous_state) => {
+                    let next_state = (*previous_state + bits + 1) % 5;
+                    *previous_state = next_state;
+                    dac.set(BIT_PATTERNS[next_state as usize]);
+                }
+                None => {
+                    previous_state = Some(bits);
+                    dac.set(BIT_PATTERNS[bits as usize]);
+                }
+            };
+
+            ticker.next().await;
+        }
+    }
+
+    dac.set(Value::Bit8(0));
 }
